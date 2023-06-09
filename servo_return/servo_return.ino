6da83d4db30d2c363ea6eab2b5cc2f806539ba1d
kawako1103 <115873701+kawako1103@users.noreply.github.com>
2023-06-09 21:24:09 +0900

    add_GPS_servo_test_code

---
+++ servo_return/servo_return.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>      // Servoライブラリの読み込み
+
+Servo myservo;          // Servoオブジェクトの宣言
+const int SV_PIN = 7;   // サーボモーターをデジタルピン7に
+
+void setup(){
+
+  myservo.attach(SV_PIN, 500, 2400);  // サーボの割当(パルス幅500~2400msに指定)
+  
+}
+
+void loop(){
+
+  myservo.write(90);    // サーボモーターを90度の位置まで動かす
+  delay(1000);
+
+  myservo.write(0);   // サーボモーターを0度の位置まで動かす
+  delay(1000);
+  exit(0);
+}
