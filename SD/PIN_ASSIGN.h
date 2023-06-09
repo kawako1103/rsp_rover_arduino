6b224d44b66a9fa8e2df1d3770c1d8a18c514d76
kawako1103 <115873701+kawako1103@users.noreply.github.com>
2023-06-10 04:45:42 +0900

    SD_card

---
+++ SD/PIN_ASSIGN.h
@@ -0,0 +1,26 @@
+#ifndef PIN_ASSIGN_H
+#define PIN_ASSIGN_H
+
+//#define PIN_SRV 4
+
+//#define PIN_LIT A1
+//#define PIN_MIC A2
+
+//#define PIN_GPS_TX 8
+//#define PIN_GPS_RX 9
+//#define PIN_XBEE_TX 2
+//#define PIN_XBEE_RX 3
+#define PIN_SD_SS  10
+#define PIN_SD_MOSI 11
+#define PIN_SD_MISO 12
+#define PIN_SD_CLK  13
+
+//#define PIN_CAM2_SS 7
+
+//#define PIN_DC_MOTER1 5
+//#define PIN_DC_MOTER2 6
+
+ #define SDA A4
+ #define SCL A5
+
+#endif
